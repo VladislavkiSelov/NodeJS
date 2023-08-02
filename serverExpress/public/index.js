d119aea79a3a633470f98f29ff41cd91925d4acc
[VladislavkiSelov] <[vladislavkiselev.2007@gmail.com]>
2023-08-02 21:59:37 +0300

    add 1

---
+++ serverExpress/public/index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.price').forEach(item => {
+    item.textContent = new Intl.NumberFormat('ru-RU',
+        {
+            currency: 'rub',
+            style: 'currency'
+        }
+    ).format(item.textContent)
+})
